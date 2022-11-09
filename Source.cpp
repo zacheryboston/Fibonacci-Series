05b846d0b36556aec746d01c919a341345a08289
Zach Boston <66221211+zacheryboston@users.noreply.github.com>
2022-11-08 18:12:43 -0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n, t1 = 0, t2 = 1, nextterm = 0;
+	cout << "Enter the number of terms: ";
+	cin >> n;
+
+	cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i) {
+        
+
+        if (i == 1) {
+            cout << t1 << ", ";
+            continue;
+        }
+        if (i == 2) {
+            cout << t2 << ", ";
+            continue;
+        }
+        nextterm = t1 + t2;
+        t1 = t2;
+        t2 = nextterm;
+
+        cout << nextterm << ", ";
+    }
+    return 0;
+}
+
+
+
